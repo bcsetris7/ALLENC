ce2e9f45c0b8fd4ae8092cc74e7cab2b0ac5322e
Bcset ris7 <102164567+bcsetris7@users.noreply.github.com>
2022-05-04 20:40:38 +0700

    Create enc.py

---
+++ BASH/enc.py
@@ -0,0 +1,76 @@
+# code by bcsetris7 | youtube : bcset ris76
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import fileinput
+
+N = '\033[0m'
+D = '\033[90m'
+W = '\033[1;37m'
+B = '\033[1;34m'
+R = '\033[1;31m'
+G = '\033[1;32m'
+Y = '\033[1;33m'
+C = '\033[1;36m'
+
+ask = G + '[' + W + '?' + G + '] '
+sukses = G + '[' + W + '√' + G + '] '
+eror = R + '[' + W + '!' + R + ']'
+
+banner = """
+di program oleh : bcset ris7 | youtube : bcset ris7
+""".format(D,W,D,W,D,W,Y,W,D,W,D,W,D,W,D,W,D,Y,D,W,D,Y,D,G,W,G,D,G,W,G,Y,D,Y,D,Y,D,Y,D,Y)
+
+banner2 = """
+   {}[{}1{}]{} Encript      {}[{}2{}]{} Decrypt
+""".format(G,W,G,W,G,W,G,W)
+
+print banner
+print banner2
+
+def dekrip():
+   try:
+       sc = raw_input(ask + W + "Script " + G + "> " + W)
+       f = open(sc,'r')
+       filedata = f.read()
+       f.close()
+
+       newdata = filedata.replace("eval","echo")
+
+       out = raw_input(ask + W + "Output" + G + " > " + W)
+       f = open(out,'w')
+       f.write(newdata)
+       f.close()
+
+       os.system("touch enc.sh")
+       os.system("bash " + out + " > tes.sh")
+       os.remove(out)
+       os.rename("enc.sh", out)
+       print (sukses + "Done..")
+
+   except KeyboardInterrupt:
+       print (eror + " Stopped!")
+   except IOError:
+       print (eror + " File Not Found!")
+
+def enkrip():
+   try:
+       script = raw_input(ask + W + "Script " + G + "> " + W)
+       output = raw_input(ask + W + "Output " + G + "> " + W)
+       os.system("bash-obfuscate " + script + " -o " + output )
+       print (sukses + "Done..")
+   except KeyboardInterrupt:
+       print (eror + " Stopped!")
+   except IOError:
+       print (eror + " File Not Found!")
+
+
+ris = raw_input(W + "Choose" + G + " > ")
+
+if ris == "1" or ris == "01":
+   enkrip()
+elif ris == "2" or ris == "02":
+   dekrip()
+else:
+   print (eror + " Wrong input")
